2b060f00bcacbca5909c7f08e5dc8a48ba55041b
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-09 20:22:10 +0500

    Task3

---
+++ Tyuiu.ChashkovTA.Sprint3.Task3.V6.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ChashkovTA.Sprint3.Task3.V6.Lib
+{
+    public class DataService : ISprint3Task3V6
+    {
+        public string ReplaceNumOnChar(string value, char item)
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in value)
+            {
+                if (char.IsDigit(c))
+                {
+                    sb.Append(item);
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tyuiu.ChashkovTA.Sprint3.Task3.V6/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ChashkovTA.Sprint3.Task3.V6.Lib;
+namespace Tyuiu.ChashkovTA.Sprint3.Task3.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #6                                                            *");
+            Console.WriteLine("* Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.ReplaceNumOnChar("12able 1o s4gh", 't');
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
