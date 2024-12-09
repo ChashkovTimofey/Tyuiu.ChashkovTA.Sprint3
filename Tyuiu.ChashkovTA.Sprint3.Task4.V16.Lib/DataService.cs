e042b9dccc0e5bc7f1591bfd796ca58515e6547d
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-09 20:40:57 +0500

    Task4

---
+++ Tyuiu.ChashkovTA.Sprint3.Task4.V16.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ChashkovTA.Sprint3.Task4.V16.Lib
+{
+    public class DataService : ISprint3Task4V16
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double product = 1;
+            for (double x = startValue; x <= stopValue; x += 0.1) 
+            {
+                if (x == 0) continue; 
+                double y = CalculateY(x);
+                product *= y;
+            }
+            return product;
+        }
+
+        public static double CalculateY(double x)
+        {
+            return (Math.Cos(x) + x) / x + 0.25;
+        }
+    }
+    
+}
---
+++ Tyuiu.ChashkovTA.Sprint3.Task4.V16/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ChashkovTA.Sprint3.Task4.V16.Lib;
+namespace Tyuiu.ChashkovTA.Sprint3.Task4.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(1, 1);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
