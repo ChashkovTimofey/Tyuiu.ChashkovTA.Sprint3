19b85cc4f06b627b2d10b5a682b00d15a15c09e5
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-09 19:33:11 +0500

    Add project files.

---
+++ Tyuiu.ChashkovTA.Sprint3.Task0.V18.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ChashkovTA.Sprint3.Task0.V18.Lib
+{
+    public class DataService : ISprint3Task0V18
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double product = 1;
+            for (int k = 1; k <= 6; k++)
+            {
+                product *= Math.Pow((2 / (Math.Cos(k) + 0.5)), k);
+            }
+            return product;
+        }
+    }
+}
