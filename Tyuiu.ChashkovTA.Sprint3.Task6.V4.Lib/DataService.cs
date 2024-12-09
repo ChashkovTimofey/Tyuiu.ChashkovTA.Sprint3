8a7f5430e5171643e613f175235ad1abbabac9f2
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-09 21:05:46 +0500

    Task6

---
+++ Tyuiu.ChashkovTA.Sprint3.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ChashkovTA.Sprint3.Task6.V4.Lib
+{
+    public class DataService : ISprint3Task6V4
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int totalCount = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                totalCount += CountDivisorsGreaterThanThreshold(i, 9);
+            }
+            return totalCount;
+        }
+
+        public static int CountDivisorsGreaterThanThreshold(int number, int threshold)
+        {
+            int count = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0 && i > threshold)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.ChashkovTA.Sprint3.Task6.V4/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ChashkovTA.Sprint3.Task6.V4.Lib;
+namespace Tyuiu.ChashkovTA.Sprint3.Task6.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #4                                                            *");
+            Console.WriteLine("* Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetSumTheDivisors(1, 1);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
